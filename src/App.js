6279e7a697418460d4232aa0b908b7be879f0737
JonLyn <jonlyn22@gmail.com>
2020-12-05 15:35:02 -0500

    setting up API :key:

--- src/App.js
+++ src/App.js
@@ -1,25 +1,35 @@
-import logo from './logo.svg';
 import './App.css';
+import { Component } from 'react';
+import axios from 'axios';
+// import firebase from 'firebase';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      books: []
+    }
+  }
+
+  componentDidMount() {
+    axios({
+      url: 'https://www.googleapis.com/books/v1/volumes',
+      method: 'GET',
+      responseType: 'jsonp',
+      params: {
+        q: 'harrypotter',
+        key: 'AIzaSyCl7_ThiCPhoMTkiOf6PNAzfAK-b-pAlXA',
+      }
+    }).then ((results) => {
+      console.log(results);
+    })
+  }
+
+  render() { 
+      return (
+          <h1>Hi</h1>
+      )
+  }
 }
 
 export default App;

ff5bb32a3c2bd0ff95df41c6d83ef1a5956c1320
JonLyn <jonlyn22@gmail.com>
2020-12-07 14:30:18 -0500

    merged from main

--- src/App.js
+++ src/App.js
@@ -26,6 +26,7 @@ class App extends Component {
       }
     }).then ((results) => {
       const bookResults = results.data.items;
+      console.log(bookResults);
       this.setState({
         books: bookResults
       })

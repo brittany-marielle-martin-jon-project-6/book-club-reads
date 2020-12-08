e51ace44ab6ff0d19fe0b84d685b0bef8b8a985c
Brittany <brittanyabarnard@gmail.com>
2020-12-08 15:02:06 -0500

    HeaderNav and landingPage additions

--- src/LandingPage.js
+++ src/LandingPage.js
@@ -4,8 +4,8 @@ import firebase from './firebase.js';
 class LandingPage extends Component {
     render() {
         return(
-            <div className="landingPageImage">
-                <h2></h2>
+            <div className="landingPage">
+                <h2>Create the reading list of your dreams and track your progress!</h2>
             </div>
         )
     }

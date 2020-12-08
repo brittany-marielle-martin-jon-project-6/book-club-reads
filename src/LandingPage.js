8a3e814ea5844b57738ef51708aa384ada0ac0ef
Brittany <brittanyabarnard@gmail.com>
2020-12-08 15:31:01 -0500

    landing page styling

--- src/LandingPage.js
+++ src/LandingPage.js
@@ -5,7 +5,7 @@ class LandingPage extends Component {
     render() {
         return(
             <div className="landingPage">
-                <h2>Create the reading list of your dreams and track your progress!</h2>
+                <h2 className="container">Create the reading list of your dreams and track your progress!</h2>
             </div>
         )
     }
